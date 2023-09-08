3f4ae3666c13bca7d66e3ac9fab559b7b3d1390f
Matt Cooper <vtbassmatt@gmail.com>
2023-09-08 16:27:52 -0700

    rotate between several images

--- tktest.py
+++ tktest.py
@@ -1,5 +1,5 @@
 import math
-
+import random
 from tkinter import Tk, ttk
 
 from PIL import Image
@@ -10,10 +10,53 @@ BUTTON_PADDING = -2 # whatever it takes to totally remove padding
 
 WORK_DELAY_MS = 200 # how many milliseconds to wait to perform next work
 
+IMAGES = [
+    'assets/microscope.webp',
+    'assets/orrery.jpeg',
+    'assets/test_tube.webp',
+]
 
-def perform_work():
-    print('did some work!')
-    root.after(WORK_DELAY_MS, perform_work)
+
+def get_window_size(root: Tk):
+    geom = root.geometry()
+    # root.geometry() gives back a string like "1280x800+0+0"
+    # we want to drop the bits after '+' and then split at the 'x'
+    # and also we want to account for the window and button padding
+    width_height, _ = geom.split('+', 1)
+    width, height = [int(x) - 2*WINDOW_PADDING for x in width_height.split('x', 1)]
+    return width, height
+
+
+def get_image(window_width: int, window_height: int):
+    filename = random.choice(IMAGES)
+    im = Image.open(filename)
+    im_width, im_height = im.size
+
+    # unpacking the logic below...
+    #   ratio of width = image width / available width
+    #   ratio of height = image height / available height
+    #   if either ratio is > 1, we are shrinking, so pick the larger one to make sure we fit
+    #   if both ratios are < 1, we would need to grow, and we don't want to do that
+    # use the largest number as a scale factor if it's >= 1
+    scale_factor = max(im_width / window_width, im_height / window_height)
+    target_width = window_width if scale_factor < 1 else math.floor(im_width / scale_factor)
+    target_height = window_height if scale_factor < 1 else math.floor(im_height / scale_factor)
+    im_resized = im.resize((target_width, target_height))
+    return PhotoImage(im_resized)
+
+
+def maybe_rotate_image(root: Tk, btn: ttk.Button):
+    # one in 10 times, we choose a new image
+    if random.random() > .9:
+        width, height = get_window_size(root)
+        tkim = get_image(width, height)
+        btn.configure(image=tkim)
+        # Python will garbage-collect the image immediately if we don't
+        # hang onto a reference like this
+        btn._keep_reference = tkim
+    
+    # schedule a future call to this method
+    root.after(WORK_DELAY_MS, lambda: maybe_rotate_image(root, btn))
 
 
 # make a full-screen window
@@ -23,45 +66,31 @@ root.attributes("-fullscreen", 1)
 
 # get the size of the window
 root.update_idletasks()
-geom = root.geometry()
-# root.geometry() gives back a string like "1280x800+0+0"
-# we want to drop the bits after '+' and then split at the 'x'
-# and also we want to account for the window and button padding
-width_height, _ = geom.split('+', 1)
-width, height = [int(x) - 2*WINDOW_PADDING for x in width_height.split('x', 1)]
+width, height = get_window_size(root)
 
-# load a photo to use, resizing as needed
-im = Image.open('assets/microscope.webp')
-im_width, im_height = im.size
-
-# unpacking the logic below...
-#   ratio of width = image width / available width
-#   ratio of height = image height / available height
-#   if either ratio is > 1, we are shrinking, so pick the larger one to make sure we fit
-#   if both ratios are < 1, we would need to grow, and we don't want to do that
-# use the largest number as a scale factor if it's >= 1
-scale_factor = max(im_width / width, im_height / height)
-target_width = width if scale_factor < 1 else math.floor(im_width / scale_factor)
-target_height = height if scale_factor < 1 else math.floor(im_height / scale_factor)
-im_resized = im.resize((target_width, target_height))
+# load the first photo to use, resizing as needed
+tkim = get_image(width, height)
 
 # put a grid onto the window
 frm = ttk.Frame(root, padding=WINDOW_PADDING)
 frm.grid()
 
 # put a photo button into the grid, clicking it will close the app
-tkim = PhotoImage(im_resized)
-ttk.Button(
-    frm, image=tkim, padding=BUTTON_PADDING,
-    command=root.destroy
-).grid(column=0, row=0)
+btn = ttk.Button(
+    frm,
+    text="Quit",
+    image=tkim,
+    padding=BUTTON_PADDING,
+    command=root.destroy,
+)
+btn.grid(column=0, row=0)
 
 # center the widgets
 root.grid_rowconfigure(0, weight=1)
 root.grid_columnconfigure(0, weight=1)
 
-# schedule outside work
-root.after(WORK_DELAY_MS, perform_work)
+# schedule image rotation
+root.after(WORK_DELAY_MS, lambda: maybe_rotate_image(root, btn))
 
 # run forever
 root.mainloop()
